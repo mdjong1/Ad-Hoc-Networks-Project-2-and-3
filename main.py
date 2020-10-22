582e6fc75216417c09d2d79a63c924a801d1d1d8
Maarten <maarten.de.jong@hotmail.com>
2020-10-22 17:05:39 +0200

    Update to more practical example
    Source can now directly reach destination, allowing for better implementation of collision later on

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import wsnsimpy.wsnsimpy_tk as wsp
 
-SOURCE = 1
-DEST = 99
+SOURCE = 0  # random.randint(0, 10)
+DEST = 15  # random.randint(90, 99)
 
 
 def delay():
@@ -35,15 +35,9 @@ class MacawNode(wsp.Node):
         self.send(wsp.BROADCAST_ADDR, msg='RTS', src=src)
 
     def send_cts(self, src):
-        if self.id is not DEST:
-            self.scene.nodecolor(self.id, 0, .7, 0)
-            self.scene.nodewidth(self.id, 2)
-        self.send(self.prev, msg='CTS', src=src)
+        self.send(wsp.BROADCAST_ADDR, msg='CTS', src=src, target=self.prev)
 
     def send_ds(self, src, length):
-        if self.id is not DEST:
-            self.scene.nodecolor(self.id, 0, 0.7, 0)
-            self.scene.nodewidth(self.id, 2)
         self.send(self.next, msg='DS', src=src, length=length)
 
     def send_data(self, src, seq):
@@ -51,9 +45,6 @@ class MacawNode(wsp.Node):
         self.send(self.next, msg='DATA', src=src, seq=seq)
 
     def send_ack(self, src):
-        if self.id is not SOURCE:
-            self.scene.nodecolor(self.id, 0, .7, 0)
-            self.scene.nodewidth(self.id, 2)
         self.send(self.prev, msg='ACK', src=src)
 
     def start_send_data(self):
@@ -85,20 +76,20 @@ class MacawNode(wsp.Node):
                 self.log(f"Send CTS to {src}")
                 self.send_cts(self.id)
 
-            else:
-                yield self.timeout(delay())
-                self.send_rts(src)
+            # else:
+            #     yield self.timeout(delay())
+            #     self.send_rts(src)
 
         elif msg == 'CTS':
-            self.next = sender
-
             if self.id is SOURCE:
+                self.next = sender
                 self.log(f"Received CTS from {src}")
                 yield self.timeout(5)
                 self.log("Start sending data")
                 self.start_process(self.start_send_data())
 
-            else:
+            elif self.id is kwargs['target']:
+                self.next = sender
                 yield self.timeout(.2)
                 self.send_cts(src)
 
@@ -123,7 +114,7 @@ class MacawNode(wsp.Node):
 
                 if seq == self._data_length:
                     self.log(f"Received all packets! {seq} of {self._data_length}")
-                    yield self.timeout(1)
+                    yield self.timeout(2)
                     self.log(f"Send ACK to {src}")
                     self.send_ack(self.id)
 
@@ -137,10 +128,10 @@ class MacawNode(wsp.Node):
 
 
 simulator = wsp.Simulator(
-    until=100,
+    until=60,
     timescale=1,
     visual=True,
-    terrain_size=(700, 700),
+    terrain_size=(650, 650),
     title="MACAW Demo"
 )
 
@@ -150,11 +141,12 @@ simulator.scene.linestyle("parent", color=(0, .8, 0), arrow="tail", width=2)
 # place nodes over 100x100 grids
 for x in range(10):
     for y in range(10):
-        px = 50 + x * 60 + random.uniform(-20, 20)
-        py = 50 + y * 60 + random.uniform(-20, 20)
-        node = simulator.add_node(MacawNode, (px, py))
-        node.tx_range = 75
-        node.logging = True
+        if x % 3 == 0 and y % 3 == 0:
+            px = 200 + x * 30 + random.uniform(-20, 20)
+            py = 200 + y * 30 + random.uniform(-20, 20)
+            node = simulator.add_node(MacawNode, (px, py))
+            node.tx_range = 450
+            node.logging = True
 
 # start the simulation
 simulator.run()
