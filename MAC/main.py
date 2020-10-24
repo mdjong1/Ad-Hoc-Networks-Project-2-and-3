94269fd013451758b251890f56ac8d4fd96b0962
Maarten <maarten.de.jong@hotmail.com>
2020-10-24 20:32:45 +0200

    Add project requirements to top of file; long way to go...

--- MAC/main.py
+++ MAC/main.py
@@ -1,6 +1,13 @@
-import random
-import time
+"""
+REQUIREMENTS:
+This group project requires you to implement one MAC protocol and show a simulation of it in action.
+The simulator should show the various nodes, and decide which nodes will send out packets at a given time.
+The implemented MAC protocol should handle the wireless communication between the various nodes.
+The number of nodes and traffic load should be configurable in the simulator to show how well the protocol performs
+under varying loads. Report your observations on different aspects such as throughput, delay, losses, etc.
+"""
 
+import random
 import wsnsimpy.wsnsimpy_tk as wsp
 
 SOURCE = 12
