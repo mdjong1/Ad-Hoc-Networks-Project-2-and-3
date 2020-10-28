5b1efbc27159934843f2d24fe6a195e6ce30b8ca
Maarten <maarten.de.jong@hotmail.com>
2020-10-28 14:09:56 +0100

    Move simulator to Topology to allow for changing of run time and title

--- MAC/LineTopology.py
+++ MAC/LineTopology.py
@@ -1,5 +1,5 @@
-from Simulator import simulator
 from MacawNode import MacawNode
+import wsnsimpy.wsnsimpy_tk as wsp
 
 
 class LineTopology:
@@ -10,7 +10,7 @@ class LineTopology:
 
     def set_nodes(self):
         for x in range(3):
-            self.nodes.append(simulator.add_node(MacawNode, (200 + (100 * x), 200)))
+            self.nodes.append(simulator.add_node(MacawNode, (225 + (100 * x), 200)))
             self.nodes[x].tx_range = 100
 
     def run(self):
@@ -20,7 +20,16 @@ class LineTopology:
 
 
 if __name__ == '__main__':
+    simulator = wsp.Simulator(
+        until=30,
+        timescale=1,
+        visual=True,
+        terrain_size=(650, 650),
+        title="MACAW Line Topology Demo"
+    )
+
     lineTop = LineTopology()
     lineTop.set_nodes()
     lineTop.run()
+
     simulator.run()
