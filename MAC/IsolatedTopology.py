1805621094582f31aeb87b3e6d01dbf1a11308a3
Maarten <maarten.de.jong@hotmail.com>
2020-11-03 12:44:58 +0100

    Include randomized isolated topology

---
+++ MAC/IsolatedTopology.py
@@ -0,0 +1,66 @@
+import random
+
+from MacawNode import MacawNode
+import wsnsimpy.wsnsimpy_tk as wsp
+
+
+NUM_NODES_PER_CLUSTER = 3
+CLUSTERS = [
+    [50, 50, 150, 150],
+    [450, 50, 600, 150],
+    [50, 450, 150, 600],
+    [450, 450, 600, 600]
+]
+
+
+class IsolatedTopology:
+
+    def __init__(self):
+        self.nodes = []
+
+    def set_nodes(self):
+        for cluster in CLUSTERS:
+            for _ in range(NUM_NODES_PER_CLUSTER):
+                self.nodes.append(
+                    simulator.add_node(
+                        MacawNode,
+                        (
+                            cluster[0] + random.randint(0, cluster[2] - cluster[0]),
+                            cluster[1] + random.randint(0, cluster[3] - cluster[1])
+                        )
+                    )
+                )
+
+        for node in self.nodes:
+            node.tx_range = 150
+
+    def run(self):
+        for cluster in range(len(CLUSTERS)):
+            print("Cluster:", cluster)
+            print("Min val:", cluster * NUM_NODES_PER_CLUSTER)
+            print("Max val:", cluster * NUM_NODES_PER_CLUSTER + NUM_NODES_PER_CLUSTER - 1)
+            min_val = cluster * NUM_NODES_PER_CLUSTER
+            max_val = cluster * NUM_NODES_PER_CLUSTER + NUM_NODES_PER_CLUSTER - 1
+            sender = self.nodes[random.randint(min_val, max_val)]
+            receiver = self.nodes[random.choice([i for i in range(min_val, max_val) if i != sender.id])].id
+            sender.send_rts(receiver)
+
+        # target = self.nodes[1].id
+        # self.nodes[0].send_rts(target)
+        # self.nodes[2].send_rts(target)
+
+
+if __name__ == '__main__':
+    simulator = wsp.Simulator(
+        until=60,
+        timescale=1,
+        visual=True,
+        terrain_size=(650, 650),
+        title="MACAW Isolated Topology Demo"
+    )
+
+    topology = IsolatedTopology()
+    topology.set_nodes()
+    topology.run()
+
+    simulator.run()
