f9cb02a22eb0873547fb096c452521729a283fb7
Maarten <maarten.de.jong@hotmail.com>
2020-11-03 13:12:39 +0100

    Add grid topology

--- MAC/GridTopology.py
+++ MAC/GridTopology.py
@@ -5,39 +5,28 @@ from MacawNode import MacawNode
 import wsnsimpy.wsnsimpy_tk as wsp
 
 TX_RANGE = 100
-NUM_SENDERS = 5
-NODE_SPACING = 50
-NUM_NODES = 30
+NUM_SENDERS = 3
+NODE_SPACING = 60
 GRID_BOUNDS = [50, 50, 600, 600]
 
 
-class RandomizedTopology:
+class GridTopology:
 
     def __init__(self):
         self.nodes = []
 
     def set_nodes(self):
-        for x in range(NUM_NODES):
-            self.nodes.append(simulator.add_node(MacawNode, self.get_pos()))
+        for x in range(GRID_BOUNDS[0], GRID_BOUNDS[2], NODE_SPACING):
+            for y in range(GRID_BOUNDS[1], GRID_BOUNDS[3], NODE_SPACING):
+                print(x, y)
+                self.nodes.append(simulator.add_node(MacawNode, (x, y)))
 
         for node in self.nodes:
             node.tx_range = TX_RANGE
 
-    def get_pos(self):
-        pos = (
-            random.randint(GRID_BOUNDS[0], GRID_BOUNDS[2]),
-            random.randint(GRID_BOUNDS[0], GRID_BOUNDS[2])
-        )
-
-        for node in self.nodes:
-            if abs(node.pos[0] - pos[0]) < NODE_SPACING and abs(node.pos[1] - pos[1]) < NODE_SPACING:
-                return self.get_pos()
-
-        return pos
-
     def get_receiver(self, sender):
         # Ensure receiver != sender
-        possible_receiver = self.nodes[random.choice([i for i in range(0, NUM_NODES - 1) if i != sender.id])]
+        possible_receiver = self.nodes[random.choice([i for i in range(0, len(self.nodes) - 1) if i != sender.id])]
 
         # Ensure receiver is within range of sender (Pythagoras)
         if math.sqrt((sender.pos[0] - possible_receiver.pos[0]) ** 2 + (sender.pos[1] - possible_receiver.pos[1]) ** 2) > TX_RANGE:
@@ -48,7 +37,8 @@ class RandomizedTopology:
 
     def run(self):
         for cluster in range(NUM_SENDERS):
-            sender = self.nodes[random.randint(0, NUM_NODES - 1)]
+            print(len(self.nodes))
+            sender = self.nodes[random.randint(0, len(self.nodes) - 1)]
 
             receiver = self.get_receiver(sender)
 
@@ -61,10 +51,10 @@ if __name__ == '__main__':
         timescale=1,
         visual=True,
         terrain_size=(650, 650),
-        title="MACAW Randomized Topology Demo"
+        title="MACAW Grid Topology Demo"
     )
 
-    topology = RandomizedTopology()
+    topology = GridTopology()
     topology.set_nodes()
     topology.run()
 
