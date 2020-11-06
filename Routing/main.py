8e08c50e5af35d41e9fb4265ce907f659d58e154
Casper Cromjongh <casper.cromjongh@hotmail.com>
2020-11-06 12:39:56 +0100

    Move message type over to separate file.

--- Routing/main.py
+++ Routing/main.py
@@ -1,6 +1,6 @@
 import random
 import wsnsimpy.wsnsimpy_tk as wsp
-from enum import Enum
+from Routing.message import MTypes, Message
 from Routing.textstyles import TStyle
 
 demo_index = 0  # Used by the demo control function
@@ -12,48 +12,9 @@ draw_arrows = True  # Whether to draw the arrows during route searching
 # TODO: do a bit more with sequence numbers
 
 
-class MTypes(Enum):
-    """Enum for possible message types"""
-    RREQ = 1
-    RREP = 2
-    RERR = 3
-    DATA = 4
-
-
 def delay(a=0.2, b=0.8):
     """Random delay between `a=0.2` and `b=0.8`"""
     return 0.3
-
-
-class Message:
-    """AODV message class"""
-
-    def __init__(self, type, src, seq, dest, hops=0):
-        """
-        :param MTypes type: Message type
-        :param int src: Source ID
-        :param int seq: Sequence ID
-        :param int dest: Destination ID
-        :param int hops: Number of hops taken
-        """
-        # if type not in message_types:
-        #     raise Exception(f"Message type ${type} not supported, possible options: ${repr(message_types)}.")
-        self.type = type
-        self.src = src
-        self.seq = seq
-        self.dest = dest
-        self.hops = hops
-
-    @classmethod
-    def from_other(cls, message):
-        """
-        :param Message cls: Class
-        :param Message message: Message object to clone
-        """
-        return Message(message.type, message.src, message.seq, message.dest, message.hops + 1)
-
-    def hop(self):
-        return Message(self.type, self.src, self.seq, self.dest, self.hops + 1)
 
 
 class MyNode(wsp.Node):
@@ -67,11 +28,7 @@ class MyNode(wsp.Node):
         """Define variables and initiate node superclass"""
         super().__init__(sim, id, pos)
 
-        # "routing table"
-        """
-        self.next = None
-        self.prev = None
-        """
+        # Routing table
         self.table = {id: {"dest": id, "next": id, "seq": 0, "hops": 0}}
         self.seq = 1
 
